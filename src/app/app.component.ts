93d93c74f73a46699a401dd7ae6044412d1a9554
oduorsamuel <samuel@techfactory.ltd>
2021-02-25 14:09:01 +0300

    Added required pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ModalService } from './modal/modal.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'electiccomerce';
+export class AppComponent implements OnInit {
+  model = {
+    email : '',
+    password: ''
+  };
+  showcart = true;
+  // tslint:disable-next-line: max-line-length
+  emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  serverErrorMessages: string;
+  constructor(private modalservice: ModalService) { }
+  ngOnInit() {
+    // if (this.userService.isLoggedIn()) {
+    // this.router.navigateByUrl('/dashboard/home');
+    // }
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    // this.userService.login(form.value).subscribe(
+    //   (res: any) => {
+    //     console.log(res);
+    //     this.userService.setToken(res.token);
+    //     const data = {
+    //       UserName: form.value.email,
+    //       LogType: 'Login'
+    //     };
+    //     this.router.navigateByUrl('/dashboard/home');
+    //   },
+    //   err => {
+    //     this.toastr.error(err.error.message);
+    //   }
+    // );
+  }
+
+  openModal(id){
+    this.modalservice.open(id);
+  }
+  closeModal(id){
+    this.modalservice.close(id);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ModalModule } from './modal';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ModalModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
